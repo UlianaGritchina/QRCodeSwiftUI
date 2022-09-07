223bd65f245eaae0663eb7107449967a2b33f8b7
Uliana Gritchina <ugritchina@icloud.com>
2022-09-07 13:47:16 +0300

    Initial Commit

---
+++ QRCodeSwiftUI/QRCodeSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRCodeSwiftUIApp.swift
+//  QRCodeSwiftUI
+//
+//  Created by Ульяна Гритчина on 07.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct QRCodeSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
