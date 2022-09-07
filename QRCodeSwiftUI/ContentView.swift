2c5a868de325462fe04960f8b070358900201fd3
Uliana Gritchina <ugritchina@icloud.com>
2022-09-07 14:55:20 +0300

    Start

--- QRCodeSwiftUI/ContentView.swift
+++ QRCodeSwiftUI/ContentView.swift
@@ -1,17 +1,37 @@
-//
-//  ContentView.swift
-//  QRCodeSwiftUI
-//
-//  Created by Ульяна Гритчина on 07.09.2022.
-//
-
 import SwiftUI
+import CoreImage.CIFilterBuiltins
 
 struct ContentView: View {
+    @State private var string = "https://github.com/UlianaGritchina"
+    let context = CIContext()
+    let filter = CIFilter.qrCodeGenerator()
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form {
+                TextField("Something", text: $string)
+                    .textContentType(.emailAddress)
+                    .font(.headline)
+                
+                Image(uiImage: generateQRCode(from: string))
+                    .interpolation(.none)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200)
+            }
+            .navigationTitle("QR")
+        }
     }
+    
+    func generateQRCode(from string: String) -> UIImage {
+        filter.message = Data(string.utf8)
+        if let outputImage = filter.outputImage {
+            if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+                return UIImage(cgImage: cgimg)
+            }
+        }
+        return UIImage(systemName: "xmark.circle") ?? UIImage()
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
