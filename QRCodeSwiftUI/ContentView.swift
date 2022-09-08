2b1c89e721e880d4ac943642f3a83f493121d89a
Uliana Gritchina <ugritchina@icloud.com>
2022-09-08 17:52:24 +0300

    GenerateButton

--- QRCodeSwiftUI/ContentView.swift
+++ QRCodeSwiftUI/ContentView.swift
@@ -2,23 +2,25 @@ import SwiftUI
 import CoreImage.CIFilterBuiltins
 
 struct ContentView: View {
-    @State private var string = "https://github.com/UlianaGritchina"
+    @State private var string = ""
+    @State private var isShowingQR = false
     let context = CIContext()
     let filter = CIFilter.qrCodeGenerator()
     var body: some View {
         NavigationView {
-            Form {
-                TextField("Something", text: $string)
-                    .textContentType(.emailAddress)
+            VStack() {
+                Text("Link, email, some text")
                     .font(.headline)
+                    .foregroundColor(.gray)
+                textEditor
                 
-                Image(uiImage: generateQRCode(from: string))
-                    .interpolation(.none)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 200, height: 200)
+                Spacer()
+                code
+                Spacer()
+                generateButton
             }
             .navigationTitle("QR")
+            .padding()
         }
     }
     
@@ -38,4 +40,41 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+extension ContentView {
+    
+    private var textEditor: some View {
+        TextEditor(text: $string)
+            .font(.headline)
+            .multilineTextAlignment(.leading)
+            .frame(width: UIScreen.main.bounds.width - 40,
+                   height: UIScreen.main.bounds.height / 3)
+            .cornerRadius(10)
+            .shadow(color: .black.opacity(0.2), radius: 5, x: 0, y: 0)
+            .ignoresSafeArea(.keyboard, edges: .all)
+    }
+    
+    private var generateButton: some View {
+        Button(action: {}) {
+            Text("Generate")
+                .font(.headline)
+                .frame(width: UIScreen.main.bounds.width - 80,
+                       height: 45)
+                .foregroundColor(.white)
+                .background(Color.blue)
+                .cornerRadius(10)
+        }
+    }
+    
+    private var code: some View {
+        Image(uiImage: generateQRCode(from: string))
+            .interpolation(.none)
+            .resizable()
+            .scaledToFit()
+            .frame(width: 200, height: 200)
+            .cornerRadius(5)
+            .shadow(radius: 5)
+    }
+    
 }
