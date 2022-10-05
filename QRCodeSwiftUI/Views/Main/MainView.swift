b38e093886963d34ba60d6d6fabe74bf89c512a1
Uliana Gritchina <ugritchina@icloud.com>
2022-10-05 08:50:45 +0300

    Custom Shapes

--- QRCodeSwiftUI/Views/Main/MainView.swift
+++ QRCodeSwiftUI/Views/Main/MainView.swift
@@ -4,6 +4,8 @@ struct MainView: View {
     @StateObject var vm = MainViewModel()
     private let height = UIScreen.main.bounds.height
     private let width = UIScreen.main.bounds.width
+    @State private var i = false
+    let eyes = ["square", "roundedRect", "roundedpointingin", "roundedouter", "leaf", "circle", "squircle"]
     var body: some View {
         ZStack {
             ScrollView(showsIndicators: false) {
@@ -16,10 +18,57 @@ struct MainView: View {
                                 selection: $vm.qrCodeColor).padding()
                     ColorPicker("Background color",
                                 selection: $vm.backgroundColor).padding()
-                    generateButton.padding(.top, height / 5)
+                    HStack {
+                        Spacer()
+                        Button(action: {
+                            withAnimation(.spring()) {
+                                i.toggle()
+                            }
+                        }) {
+                            Circle()
+                                .frame(width: width / 7)
+                                .foregroundColor(.white)
+                                .shadow(color: Color("Color"), radius: 5, x: 0, y: 0)
+                                .blur(radius: 0.5)
+                                .overlay {
+                                    Image("square")
+                                        .resizable()
+                                        .frame(width: 25, height: 25)
+                                        .foregroundColor(.gray)
+                                        .font(.headline)
+                                }
+                        }
+                        .padding(.horizontal)
+                        
+                        Button(action: {
+                            
+                        }) {
+                            Circle()
+                                .frame(width: width / 7)
+                                .foregroundColor(.white)
+                                .shadow(color: Color("Color"), radius: 5, x: 0, y: 0)
+                                .blur(radius: 0.5)
+                                .overlay {
+                                    Image("square")
+                                        .resizable()
+                                        .frame(width: 25, height: 25)
+                                        .foregroundColor(.gray)
+                                        .font(.headline)
+                                }
+                        }
+                        .padding(.leading)
+                        
+                    }
+                    .padding()
                 }
                 .padding(.horizontal)
             }
+            
+            VStack {
+                Spacer()
+                generateButton.padding(.bottom, 30)
+            }
+            .ignoresSafeArea()
             
             blackView
             QRCodeSheetView(
@@ -29,6 +78,65 @@ struct MainView: View {
                     color2: vm.backgroundColor),
                 isShowingQR: $vm.isShowingQR
             )
+            
+            VStack {
+                Spacer()
+                RoundedRectangle(cornerRadius: 10)
+                    .frame(width: width, height: height / 3)
+                    .foregroundColor(.white)
+                    .overlay {
+                        VStack {
+                            Text("Eye")
+                                .font(.headline)
+                            Spacer()
+                            HStack {
+                                ForEach(0..<4) { index in
+                                    Circle()
+                                        .frame(width: width / 7)
+                                        .foregroundColor(.white)
+                                        .shadow(color: Color("Color"), radius: 5, x: 0, y: 0)
+                                        .blur(radius: 0.5)
+                                        .overlay {
+                                            Image(eyes[index])
+                                                .resizable()
+                                                .frame(width: 25, height: 25)
+                                                .foregroundColor(.gray)
+                                                .font(.headline)
+                                        }
+                                        .padding(.horizontal)
+                                }
+                            }
+                            .padding()
+                            HStack {
+                                ForEach(4..<7) { index in
+                                    Circle()
+                                        .frame(width: width / 7)
+                                        .foregroundColor(.white)
+                                        .shadow(color: Color("Color"), radius: 5, x: 0, y: 0)
+                                        .blur(radius: 0.5)
+                                        .overlay {
+                                            Image(eyes[index])
+                                                .resizable()
+                                                .frame(width: 25, height: 25)
+                                                .foregroundColor(.gray)
+                                                .font(.headline)
+                                        }
+                                        .padding(.horizontal)
+                                }
+                            }
+                            Spacer()
+                            
+                        }
+                        .padding()
+                    }
+            }
+            .offset(y: i ? 0 : height)
+            .onTapGesture {
+                withAnimation(.spring()) {
+                    i.toggle()
+                }
+            }
+            .ignoresSafeArea()
             
         }
         .navigationTitle("QR")
@@ -78,7 +186,7 @@ extension MainView {
         Rectangle()
             .ignoresSafeArea()
             .foregroundColor(.black)
-            .opacity(vm.isShowingQR ? 0.3 : 0)
+            .opacity(vm.isShowingQR ? 0.4 : i ? 0.4 : 0)
     }
     
     private var doneButtonForToolBar: some View {
