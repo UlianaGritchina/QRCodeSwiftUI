f7fac9c286246337dd1d43f1daff982be32884ee
Uliana Gritchina <ugritchina@icloud.com>
2022-10-06 17:00:47 +0300

    Eye and data ui

--- QRCodeSwiftUI/Views/Main/MainView.swift
+++ QRCodeSwiftUI/Views/Main/MainView.swift
@@ -4,8 +4,7 @@ struct MainView: View {
     @StateObject var vm = MainViewModel()
     private let height = UIScreen.main.bounds.height
     private let width = UIScreen.main.bounds.width
-    @State private var i = false
-    let eyes = ["square", "roundedRect", "roundedpointingin", "roundedouter", "leaf", "circle", "squircle"]
+    
     var body: some View {
         ZStack {
             ScrollView(showsIndicators: false) {
@@ -18,48 +17,9 @@ struct MainView: View {
                                 selection: $vm.qrCodeColor).padding()
                     ColorPicker("Background color",
                                 selection: $vm.backgroundColor).padding()
-                    HStack {
-                        Spacer()
-                        Button(action: {
-                            withAnimation(.spring()) {
-                                i.toggle()
-                            }
-                        }) {
-                            Circle()
-                                .frame(width: width / 7)
-                                .foregroundColor(.white)
-                                .shadow(color: Color("Color"), radius: 5, x: 0, y: 0)
-                                .blur(radius: 0.5)
-                                .overlay {
-                                    Image("square")
-                                        .resizable()
-                                        .frame(width: 25, height: 25)
-                                        .foregroundColor(.gray)
-                                        .font(.headline)
-                                }
-                        }
-                        .padding(.horizontal)
-                        
-                        Button(action: {
-                            
-                        }) {
-                            Circle()
-                                .frame(width: width / 7)
-                                .foregroundColor(.white)
-                                .shadow(color: Color("Color"), radius: 5, x: 0, y: 0)
-                                .blur(radius: 0.5)
-                                .overlay {
-                                    Image("square")
-                                        .resizable()
-                                        .frame(width: 25, height: 25)
-                                        .foregroundColor(.gray)
-                                        .font(.headline)
-                                }
-                        }
-                        .padding(.leading)
-                        
-                    }
-                    .padding()
+                    
+                    shapesButtons.padding()
+                    
                 }
                 .padding(.horizontal)
             }
@@ -78,65 +38,23 @@ struct MainView: View {
                     color2: vm.backgroundColor),
                 isShowingQR: $vm.isShowingQR
             )
-            
-            VStack {
-                Spacer()
-                RoundedRectangle(cornerRadius: 10)
-                    .frame(width: width, height: height / 3)
-                    .foregroundColor(.white)
-                    .overlay {
-                        VStack {
-                            Text("Eye")
-                                .font(.headline)
-                            Spacer()
-                            HStack {
-                                ForEach(0..<4) { index in
-                                    Circle()
-                                        .frame(width: width / 7)
-                                        .foregroundColor(.white)
-                                        .shadow(color: Color("Color"), radius: 5, x: 0, y: 0)
-                                        .blur(radius: 0.5)
-                                        .overlay {
-                                            Image(eyes[index])
-                                                .resizable()
-                                                .frame(width: 25, height: 25)
-                                                .foregroundColor(.gray)
-                                                .font(.headline)
-                                        }
-                                        .padding(.horizontal)
-                                }
-                            }
-                            .padding()
-                            HStack {
-                                ForEach(4..<7) { index in
-                                    Circle()
-                                        .frame(width: width / 7)
-                                        .foregroundColor(.white)
-                                        .shadow(color: Color("Color"), radius: 5, x: 0, y: 0)
-                                        .blur(radius: 0.5)
-                                        .overlay {
-                                            Image(eyes[index])
-                                                .resizable()
-                                                .frame(width: 25, height: 25)
-                                                .foregroundColor(.gray)
-                                                .font(.headline)
-                                        }
-                                        .padding(.horizontal)
-                                }
-                            }
-                            Spacer()
-                            
-                        }
-                        .padding()
+            eyesView
+                .offset(y: vm.isShowingEyes ? 0 : height)
+                .onTapGesture {
+                    withAnimation(.spring()) {
+                        vm.isShowingEyes.toggle()
                     }
-            }
-            .offset(y: i ? 0 : height)
-            .onTapGesture {
-                withAnimation(.spring()) {
-                    i.toggle()
                 }
-            }
-            .ignoresSafeArea()
+                .ignoresSafeArea()
+            
+            dataView
+                .offset(y: vm.isShowingData ? 0 : height)
+                .onTapGesture {
+                    withAnimation(.spring()) {
+                        vm.isShowingData.toggle()
+                    }
+                }
+                .ignoresSafeArea()
             
         }
         .navigationTitle("QR")
@@ -165,7 +83,7 @@ extension MainView {
         TextEditor(text: $vm.text)
             .font(.headline)
             .multilineTextAlignment(.leading)
-            .frame(width: width - 40, height: height / 3)
+            .frame(width: width - 40, height: height / 4)
             .cornerRadius(10)
             .shadow(color: Color("Color"), radius: 5, x: 0, y: 0)
     }
@@ -186,7 +104,13 @@ extension MainView {
         Rectangle()
             .ignoresSafeArea()
             .foregroundColor(.black)
-            .opacity(vm.isShowingQR ? 0.4 : i ? 0.4 : 0)
+            .opacity(vm.isShowingQR
+                     ? 0.4
+                     : vm.isShowingEyes
+                     ? 0.4
+                     : vm.isShowingData
+                     ? 0.4
+                     : 0)
     }
     
     private var doneButtonForToolBar: some View {
@@ -204,4 +128,185 @@ extension MainView {
         }
     }
     
+    private var shapesButtons: some View {
+        HStack {
+            Spacer()
+            VStack {
+                Button(action: {
+                    withAnimation(.spring()) {
+                        vm.isShowingEyes.toggle()
+                    }
+                }) {
+                    Circle()
+                        .frame(width: width / 7)
+                        .foregroundColor(.white)
+                        .shadow(color: Color("Color"), radius: 5, x: 0, y: 0)
+                        .blur(radius: 0.5)
+                        .overlay {
+                            Image(vm.selectedEye)
+                                .resizable()
+                                .frame(width: 28, height: 28)
+                        }
+                }
+                .padding(.horizontal)
+                
+                Text("Eye")
+            }
+            
+            VStack {
+                Button(action: {
+                    withAnimation(.spring()) {
+                        vm.isShowingData.toggle()
+                    }
+                }) {
+                    Circle()
+                        .frame(width: width / 7)
+                        .foregroundColor(.white)
+                        .shadow(color: Color("Color"), radius: 5, x: 0, y: 0)
+                        .blur(radius: 0.5)
+                        .overlay {
+                            Image(vm.selectedData)
+                                .resizable()
+                                .frame(width: 28, height: 28)
+                        }
+                }
+                Text("Data")
+            }
+        }
+    }
+    
+    private var eyesView: some View {
+        VStack {
+            Spacer()
+            RoundedRectangle(cornerRadius: 10)
+                .frame(width: width, height: height / 3)
+                .foregroundColor(.white)
+                .overlay {
+                    VStack {
+                        Text("Eye")
+                            .font(.headline)
+                        Spacer()
+                        HStack {
+                            ForEach(0..<3) { index in
+                                Button(action: {
+                                    vm.selectedEye = vm.eyes[index]
+                                    withAnimation(.spring()) {
+                                        vm.isShowingEyes.toggle()
+                                    }
+                                }) {
+                                    Circle()
+                                        .frame(width: width / 7)
+                                        .foregroundColor(.white)
+                                        .shadow(color: Color("Color"),
+                                                radius: 5, x: 0, y: 0)
+                                        .blur(radius: 0.5)
+                                        .overlay {
+                                            Image(vm.eyes[index])
+                                                .resizable()
+                                                .frame(width: 30, height: 30)
+                                        }
+                                }
+                                .padding(.horizontal)
+                            }
+                        }
+                        .padding()
+                        HStack {
+                            ForEach(3..<7) { index in
+                                Button(action: {
+                                    vm.selectedEye = vm.eyes[index]
+                                    withAnimation(.spring()) {
+                                        vm.isShowingEyes.toggle()
+                                    }
+                                }) {
+                                    Circle()
+                                        .frame(width: width / 7)
+                                        .foregroundColor(.white)
+                                        .shadow(color: Color("Color"),
+                                                radius: 5, x: 0, y: 0)
+                                        .blur(radius: 0.5)
+                                        .overlay {
+                                            Image(vm.eyes[index])
+                                                .resizable()
+                                                .frame(width: 30, height: 30)
+                                                .foregroundColor(.gray)
+                                        }
+                                }
+                                .padding(.horizontal)
+                            }
+                        }
+                        Spacer()
+                        
+                    }
+                    .padding()
+                }
+        }
+    }
+    
+    
+    private var dataView: some View {
+        VStack {
+            Spacer()
+            RoundedRectangle(cornerRadius: 10)
+                .frame(width: width, height: height / 3)
+                .foregroundColor(.white)
+                .overlay {
+                    VStack {
+                        Text("Data")
+                            .font(.headline)
+                        Spacer()
+                        HStack {
+                            ForEach(0..<2) { index in
+                                Button(action: {
+                                    vm.selectedData = vm.data[index]
+                                    withAnimation(.spring()) {
+                                        vm.isShowingData.toggle()
+                                    }
+                                }) {
+                                    Circle()
+                                        .frame(width: width / 7)
+                                        .foregroundColor(.white)
+                                        .shadow(color: Color("Color"),
+                                                radius: 5, x: 0, y: 0)
+                                        .blur(radius: 0.5)
+                                        .overlay {
+                                            Image(vm.data[index])
+                                                .resizable()
+                                                .frame(width: 30, height: 30)
+                                        }
+                                }
+                                .padding(.horizontal)
+                            }
+                        }
+                        .padding()
+                        HStack {
+                            ForEach(2..<5) { index in
+                                Button(action: {
+                                    vm.selectedData = vm.data[index]
+                                    withAnimation(.spring()) {
+                                        vm.isShowingData.toggle()
+                                    }
+                                }) {
+                                    Circle()
+                                        .frame(width: width / 7)
+                                        .foregroundColor(.white)
+                                        .shadow(color: Color("Color"),
+                                                radius: 5, x: 0, y: 0)
+                                        .blur(radius: 0.5)
+                                        .overlay {
+                                            Image(vm.data[index])
+                                                .resizable()
+                                                .frame(width: 30, height: 30)
+                                                .foregroundColor(.gray)
+                                        }
+                                }
+                                .padding(.horizontal)
+                            }
+                        }
+                        Spacer()
+                        
+                    }
+                    .padding()
+                }
+        }
+    }
 }
--- QRCodeSwiftUI/Views/Main/MainViewModel.swift
+++ QRCodeSwiftUI/Views/Main/MainViewModel.swift
@@ -7,6 +7,26 @@ class MainViewModel: ObservableObject {
     @Published var isShowingQR = false
     @Published var qrCodeColor: Color = .black
     @Published var backgroundColor: Color = .white
+    @Published var selectedEye = "square"
+    @Published var selectedData = "squareData"
+    @Published var isShowingEyes = false
+    @Published var isShowingData = false
+    
+    let eyes = ["square",
+                "roundedRect",
+                "roundedPointingIn",
+                "roundedOuter",
+                "leaf",
+                "circle",
+                "squircle",
+                "barsVertical",
+                "barsHorizontal"]
+    
+    let data = ["curvePixelData",
+                "roundedPathData",
+                "roundedOuterData",
+                "circleData",
+                "squareData"]
     
     func showQRCodeView() {
         isShowingQR.toggle()
