9c6f29eb445a4fe51d6cd35f7a1cf2039942d9d9
Uliana Gritcihna <ugritchina@icloud.com>
2023-11-13 10:34:28 +0300

    Saving qr.

--- QRCodeSwiftUI/Core/Main/GeneratedQRView/GeneratedQRView.swift
+++ QRCodeSwiftUI/Core/Main/GeneratedQRView/GeneratedQRView.swift
@@ -7,8 +7,46 @@
 
 import SwiftUI
 
+@MainActor final class GeneratedQRViewModel: ObservableObject {
+    
+    @Published var qrTitle = ""
+    @Published var isShowAlert = false
+    
+    var codes: [QRCode] = []
+    
+    init() {
+        getItems()
+    }
+    
+    func showAlert() {
+        isShowAlert = true
+    }
+    
+    func addCode(_ code: QRCode) {
+        codes.append(code)
+        saveCodes()
+    }
+    
+  private func saveCodes() {
+        if let encodedData = try? JSONEncoder().encode(codes) {
+            UserDefaults.standard.set(encodedData, forKey: "codes_list")
+        }
+    }
+    
+    private func getItems() {
+        guard
+            let data = UserDefaults.standard.data(forKey: "codes_list"),
+            let savedCodes = try? JSONDecoder().decode([QRCode].self, from: data)
+        else { return }
+        
+        codes = savedCodes
+    }
+}
+
 struct GeneratedQRView: View {
     let qrCodeImageData: Data?
+    @StateObject private var vm = GeneratedQRViewModel()
+    @Environment(\.dismiss) private var dismiss
     var body: some View {
         NavigationView {
             ZStack {
@@ -16,11 +54,18 @@ struct GeneratedQRView: View {
                 VStack {
                     qrCode
                     Spacer()
-                    ButtonView(title: "Save", action: { })
+                    ButtonView(title: "Save", action: { vm.showAlert() })
                 }
                 .padding()
             }
             .navigationTitle("Generated")
+            .alert("QR title", isPresented: $vm.isShowAlert) {
+                TextField("Enter your name", text: $vm.qrTitle)
+                Button("Cancel", role: .cancel) { }
+                Button("Save", action: saveQR)
+            } message: {
+                Text("Set title for your QR code.")
+            }
         }
     }
 }
@@ -39,4 +84,9 @@ extension GeneratedQRView {
         }
     }
     
+    private func saveQR() {
+        let code = QRCode(name: vm.qrTitle, text: "", imageData: qrCodeImageData!)
+        vm.addCode(code)
+        dismiss()
+    }
 }
