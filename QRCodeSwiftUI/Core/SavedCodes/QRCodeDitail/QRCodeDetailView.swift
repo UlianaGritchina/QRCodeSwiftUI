a6a44d218cba77e2ca047ec429b0ef2be389c3fb
Uliana Gritchina <ugritchina@icloud.com>
2023-11-14 19:52:35 +0300

    CircleButton

--- QRCodeSwiftUI/Core/SavedCodes/QRCodeDitail/QRCodeDetailView.swift
+++ QRCodeSwiftUI/Core/SavedCodes/QRCodeDitail/QRCodeDetailView.swift
@@ -11,14 +11,10 @@ struct QRCodeDetailView: View {
                 QrView(data: qrCode.imageData, size: 300)
                 Spacer()
                 Spacer()
+                buttons
             }
         }
         .navigationTitle(qrCode.name)
-        .toolbar {
-            ToolbarItemGroup(placement: .navigationBarTrailing) {
-                ShareButtonView(codeImageData: qrCode.imageData, imageSize: 18)
-            }
-        }
     }
     
 }
@@ -26,7 +22,37 @@ struct QRCodeDetailView: View {
 struct QRCodeDetailView_Previews: PreviewProvider {
     static var previews: some View {
         QRCodeDetailView(
-            qrCode: QRCode(name: "name", text: "text", imageData: Data())
+            qrCode: QRCode(
+                name: "name",
+                text: "text",
+                imageData: (UIImage(named: "defaultQRImage")?.pngData())!
+            )
         )
     }
+}
+
+extension QRCodeDetailView {
+    
+    private var buttons: some View {
+        HStack {
+            Spacer()
+            CircleButton(imageName: "trash", action: { })
+            Spacer()
+            shareButton
+            Spacer()
+            CircleButton(imageName: "sun.min", action: {})
+            Spacer()
+        }
+        .padding()
+        .padding(.bottom)
+    }
+    
+    private var shareButton: some View {
+        ShareButtonView(codeImageData: qrCode.imageData, imageSize: 18)
+            .frame(width: 50, height: 50)
+            .background(Color("cardBackground"))
+            .cornerRadius(25)
+            .shadow(color: .black.opacity(0.2), radius: 5, x: 0, y: 0)
+    }
+    
 }
